1614c422f5f2279f007973dd25966b35e451bb96
LiaDala <liadala.mueller@gmail.com>
2021-06-22 00:24:54 +0200

    update

--- webserver.go
+++ webserver.go
@@ -5,92 +5,146 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"strings"
+
+	"github.com/gorilla/mux"
 )
 
 func startWebserver() {
+	router := mux.NewRouter()
+	fs := http.FileServer(http.Dir("./web"))
 
-	http.HandleFunc("/auth", func(rw http.ResponseWriter, r *http.Request) {
-		keys, ok := r.URL.Query()["code"]
-		if !ok || len(keys[0]) < 1 {
-			rw.Header().Set("Content-Type", "application/json")
-			rw.WriteHeader(http.StatusBadRequest)
-			rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
-		}
-		url := fmt.Sprint("https://id.twitch.tv/oauth2/token",
-			"?client_id="+config.ClientId,
-			"&client_secret="+config.ClientSecret,
-			"&code="+keys[0],
-			"&grant_type=authorization_code",
-			"&redirect_uri="+config.RedirectURL,
-		)
-		request, err := http.NewRequest(http.MethodPost, url, nil)
-		if err != nil {
-			rw.Header().Set("Content-Type", "application/json")
-			rw.WriteHeader(http.StatusBadRequest)
-			rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
-		}
-		client := &http.Client{}
-		response, err := client.Do(request)
-		if err != nil {
-			rw.Header().Set("Content-Type", "application/json")
-			rw.WriteHeader(http.StatusBadRequest)
-			rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
-		}
-		defer response.Body.Close()
+	router.HandleFunc("/api/refresh/{key}", refresh)
+	router.HandleFunc("api/revoke/{key}", revoke)
+	router.HandleFunc("/auth", auth)
+	router.PathPrefix("/").Handler(fs)
 
-		b, err := io.ReadAll(response.Body)
-		if err != nil {
-			rw.Header().Set("Content-Type", "application/json")
-			rw.WriteHeader(http.StatusBadRequest)
-			rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
-		}
+	//http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":80", router))
+}
 
+func revoke(rw http.ResponseWriter, r *http.Request) {
+	var (
+		err  error
+		vars = mux.Vars(r)
+	)
+	if vars["key"] == "" {
 		rw.Header().Set("Content-Type", "application/json")
-		rw.WriteHeader(http.StatusOK)
-		rw.Write(b)
-	})
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
+	}
 
-	http.HandleFunc("/api/refresh", func(rw http.ResponseWriter, r *http.Request) {
-		var (
-		//err error
-		)
-		req := strings.ReplaceAll(r.URL.Path, "/api/refresh", "")
-		/*
-			url := fmt.Sprint("https://id.twitch.tv/oauth2/token",
-				"?client_id="+config.ClientId,
-				"&client_secret="+config.ClientSecret,
-				"&grant_type=refresh_token",
-				"&refresh_token="+req,
-			)
-			request, err := http.NewRequest(http.MethodPost, url, nil)
-			if err != nil {
-				rw.Header().Set("Content-Type", "application/json")
-				rw.WriteHeader(http.StatusBadRequest)
-				rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
-			}
-			client := &http.Client{}
-			response, err := client.Do(request)
-			if err != nil {
-				rw.Header().Set("Content-Type", "application/json")
-				rw.WriteHeader(http.StatusBadRequest)
-				rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
-			}
-			defer response.Body.Close()
-
-			b, err := io.ReadAll(response.Body)
-			if err != nil {
-				rw.Header().Set("Content-Type", "application/json")
-				rw.WriteHeader(http.StatusBadRequest)
-				rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
-			}
-		*/
+	url := fmt.Sprint("https://id.twitch.tv/oauth2/revoke",
+		"?client_id="+config.ClientId,
+		"&token="+vars["key"],
+	)
+	request, err := http.NewRequest(http.MethodPost, url, nil)
+	if err != nil {
 		rw.Header().Set("Content-Type", "application/json")
-		rw.WriteHeader(http.StatusOK)
-		//rw.Write(b)
-		rw.Write([]byte(req))
-	})
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
+	}
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		rw.Header().Set("Content-Type", "application/json")
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
+	}
+	defer response.Body.Close()
 
-	http.Handle("/", http.FileServer(http.Dir("./web")))
-	log.Fatal(http.ListenAndServe(":80", nil))
+	b, err := io.ReadAll(response.Body)
+	if err != nil {
+		rw.Header().Set("Content-Type", "application/json")
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(http.StatusOK)
+	rw.Write(b)
+}
+
+func refresh(rw http.ResponseWriter, r *http.Request) {
+	var (
+		err  error
+		vars = mux.Vars(r)
+	)
+	if vars["key"] == "" {
+		rw.Header().Set("Content-Type", "application/json")
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
+	}
+
+	url := fmt.Sprint("https://id.twitch.tv/oauth2/token",
+		"?client_id="+config.ClientId,
+		"&client_secret="+config.ClientSecret,
+		"&grant_type=refresh_token",
+		"&refresh_token="+vars["key"],
+	)
+	request, err := http.NewRequest(http.MethodPost, url, nil)
+	if err != nil {
+		rw.Header().Set("Content-Type", "application/json")
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
+	}
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		rw.Header().Set("Content-Type", "application/json")
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
+	}
+	defer response.Body.Close()
+
+	b, err := io.ReadAll(response.Body)
+	if err != nil {
+		rw.Header().Set("Content-Type", "application/json")
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(http.StatusOK)
+	rw.Write(b)
+}
+
+func auth(rw http.ResponseWriter, r *http.Request) {
+	keys, ok := r.URL.Query()["code"]
+	if !ok || len(keys[0]) < 1 {
+		rw.Header().Set("Content-Type", "application/json")
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
+	}
+	url := fmt.Sprint("https://id.twitch.tv/oauth2/token",
+		"?client_id="+config.ClientId,
+		"&client_secret="+config.ClientSecret,
+		"&code="+keys[0],
+		"&grant_type=authorization_code",
+		"&redirect_uri="+config.RedirectURL,
+	)
+	request, err := http.NewRequest(http.MethodPost, url, nil)
+	if err != nil {
+		rw.Header().Set("Content-Type", "application/json")
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
+	}
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		rw.Header().Set("Content-Type", "application/json")
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
+	}
+	defer response.Body.Close()
+
+	b, err := io.ReadAll(response.Body)
+	if err != nil {
+		rw.Header().Set("Content-Type", "application/json")
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(http.StatusOK)
+	rw.Write(b)
 }
