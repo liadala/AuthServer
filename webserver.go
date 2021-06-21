fc48a775dae9df2915afb4b41b6552a68f9358e7
LiaDala <liadala.mueller@gmail.com>
2021-06-21 22:42:24 +0200

    update

--- webserver.go
+++ webserver.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func startWebserver() {
@@ -48,6 +49,46 @@ func startWebserver() {
 		rw.Header().Set("Content-Type", "application/json")
 		rw.WriteHeader(http.StatusOK)
 		rw.Write(b)
+	})
+
+	http.HandleFunc("/api/refresh", func(rw http.ResponseWriter, r *http.Request) {
+		var (
+		//err error
+		)
+		req := strings.ReplaceAll(r.URL.Path, "/api/refresh", "")
+		/*
+			url := fmt.Sprint("https://id.twitch.tv/oauth2/token",
+				"?client_id="+config.ClientId,
+				"&client_secret="+config.ClientSecret,
+				"&grant_type=refresh_token",
+				"&refresh_token="+req,
+			)
+			request, err := http.NewRequest(http.MethodPost, url, nil)
+			if err != nil {
+				rw.Header().Set("Content-Type", "application/json")
+				rw.WriteHeader(http.StatusBadRequest)
+				rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
+			}
+			client := &http.Client{}
+			response, err := client.Do(request)
+			if err != nil {
+				rw.Header().Set("Content-Type", "application/json")
+				rw.WriteHeader(http.StatusBadRequest)
+				rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
+			}
+			defer response.Body.Close()
+
+			b, err := io.ReadAll(response.Body)
+			if err != nil {
+				rw.Header().Set("Content-Type", "application/json")
+				rw.WriteHeader(http.StatusBadRequest)
+				rw.Write([]byte("{\"status\":\"Bad Request\",\"stauscode\":400}"))
+			}
+		*/
+		rw.Header().Set("Content-Type", "application/json")
+		rw.WriteHeader(http.StatusOK)
+		//rw.Write(b)
+		rw.Write([]byte(req))
 	})
 
 	http.Handle("/", http.FileServer(http.Dir("./web")))
